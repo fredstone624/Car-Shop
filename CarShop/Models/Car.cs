7d3ecc07c257ab2252c7c201b3b3f822abed30e3
Fredstone <fredstone624@gmail.com>
2018-07-29 00:05:22 +0300

    add models (entities)

---
+++ CarShop/Models/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace CarShop.Models
+{
+    public partial class Car
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public string Preview { get; set; }
+        public string SubPreview { get; set; }
+        public int Count { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ CarShop/Models/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace CarShop.Models
+{
+    public partial class Order
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public int CarId { get; set; }
+        public string Status { get; set; }
+    }
+}
