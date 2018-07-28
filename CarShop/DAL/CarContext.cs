0253a10b699c2b3f598cb3c3c6e62ec6a14a8553
Fredstone <fredstone624@gmail.com>
2018-07-29 01:04:20 +0300

    add context + initializer + connectionString

---
+++ CarShop/DAL/CarContext.cs
@@ -0,0 +1,13 @@
+﻿using CarShop.Models;
+using System.Data.Entity;
+
+namespace CarShop.DAL
+{
+    public class CarContext : DbContext
+    {
+        CarContext() : base("CarsConnection") { }
+
+        public DbSet<Car> Cars { set; get; }
+        public DbSet<Order> Orders { set; get; }
+    }
+}
---
+++ CarShop/DAL/CarInitializer.cs
@@ -0,0 +1,45 @@
+﻿using CarShop.Models;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace CarShop.DAL
+{
+    public class CarInitializer : DropCreateDatabaseIfModelChanges<CarContext>
+    {
+        protected override void Seed(CarContext context)
+        {
+            var cars = new List<Car>
+            {
+                new Car { Title = "Audi", Type = "Седан", Description = "Описание ауди", Preview = "/Content/img/cars/audi-itm.jpg",
+                SubPreview = "/Content/img/cars/audi-itm.jpg,/Content/img/cars/audi-itm.jpg,/Content/img/cars/audi-itm.jpg",
+                Count = 10, Price = 300000 },
+
+                new Car { Title = "Audi", Type = "Седан", Description = "Описание ауди", Preview = "/Content/img/cars/audi-itm.jpg",
+                SubPreview = "/Content/img/cars/audi-itm.jpg,/Content/img/cars/audi-itm.jpg,/Content/img/cars/audi-itm.jpg",
+                Count = 10, Price = 300000 },
+
+                new Car { Title = "Audi", Type = "Седан", Description = "Описание ауди", Preview = "/Content/img/cars/audi-itm.jpg",
+                SubPreview = "/Content/img/cars/audi-itm.jpg,/Content/img/cars/audi-itm.jpg,/Content/img/cars/audi-itm.jpg",
+                Count = 10, Price = 300000 }
+            };
+
+            cars.ForEach(c => context.Cars.Add(c));
+            context.SaveChanges();
+
+            var orders = new List<Order>
+            {
+                new Order { UserName = "Mark", UserEmail = "mark@mail.ru", CarId = 1,
+                Status = "В обработке"},
+
+                new Order { UserName = "Frank", UserEmail = "frank@mail.ru", CarId = 3,
+                Status = "В обработке"},
+
+                new Order { UserName = "Ros", UserEmail = "ros@mail.ru", CarId = 2,
+                Status = "В обработке"}
+            };
+
+            orders.ForEach(o => context.Orders.Add(o));
+            context.SaveChanges();
+        }
+    }
+}
