3a22b8fbd46f5f859928c6cd9476df747ce456fb
Fredstone <fredstone624@gmail.com>
2018-07-29 11:20:40 +0300

    edit homeController

--- CarShop/Controllers/HomeController.cs
+++ CarShop/Controllers/HomeController.cs
@@ -1,30 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using CarShop.DAL;
 
 namespace CarShop.Controllers
 {
     public class HomeController : Controller
     {
+        private CarContext db = new CarContext();
+
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+            return View(db.Cars);
+        }        
     }
 }
--- CarShop/DAL/CarContext.cs
+++ CarShop/DAL/CarContext.cs
@@ -5,7 +5,7 @@ namespace CarShop.DAL
 {
     public class CarContext : DbContext
     {
-        CarContext() : base("CarsConnection") { }
+        public CarContext() : base("CarsConnection") { }
 
         public DbSet<Car> Cars { set; get; }
         public DbSet<Order> Orders { set; get; }
