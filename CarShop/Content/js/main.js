6f7478b365f5881e91389a59b72f8bafed0697e1
Fredstone <fredstone624@gmail.com>
2018-08-07 10:10:04 +0300

    form

---
+++ CarShop/Content/js/main.js
@@ -0,0 +1,21 @@
+﻿function SubmitForm(name, email, car) {
+    $.ajax({
+        method: "POST",
+        url: "/Home/Form",
+        data: { Name: name, Email: email, Car: car }
+    })
+        .done(function (msg) {
+            alert("Отлично: " + msg);
+        }).fail(function () {
+            alert("При передачи данных произошла ошибка");
+        });
+}
+
+$("#form").submit(function () {
+
+    var UserName    = $('input[name=Name]').val();
+    var UserEmail   = $('input[name=Email]').val();
+    var Car         = $('select[name=Car]').val();
+
+    SubmitForm(UserName, UserEmail, Car);
+});
--- CarShop/Controllers/HomeController.cs
+++ CarShop/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using System.Web.Mvc;
 using CarShop.DAL;
+using CarShop.Models;
 
 namespace CarShop.Controllers
 {
@@ -27,12 +28,52 @@ namespace CarShop.Controllers
         public ActionResult Nav()
         {
             var cars = db.Cars;
-            string result = string.Empty;
+            string result = "";
             foreach(var car in cars)
             {
                 result += string.Format("<li><a href='/Home/Car/?car_id={0}'>{1}</a></li>", car.Id, car.Title);
             }
             return Content(result);
+        }
+
+        [HttpGet]
+        public ActionResult Form(int car_id = 1)
+        {
+            ViewBag.Car = car_id;
+            return PartialView();
+        }
+
+        [ChildActionOnly]
+        public ActionResult FormOptions(int car_id = 1)
+        {
+            var cars = db.Cars;
+            string options = "";
+            foreach (var car in cars)
+            {
+                if (car.Id == car_id)
+                    options += string.Format("<option value='{0}' selected>{1}</option>", car.Id, car.Title);
+                else
+                    options += string.Format("<option value='{0}'>{1}</option>", car.Id, car.Title);
+
+            }
+            return Content(options);
+        }
+
+        [HttpPost]
+        public ActionResult Form(string userName, string userEmail, int carId)
+        {
+            var order = new Order
+            {
+                UserName = userName,
+                UserEmail = userEmail,
+                CarId = carId,
+                Status = "Создана"
+            };
+
+            db.Orders.Add(order);
+            db.SaveChanges();
+
+            return Content(string.Format("{0}, ваша заявка на автомобиль {1}, была принята!", userName, db.Cars.FirstOrDefault(c => c.Id == carId).Title));
         }
     }
 }
